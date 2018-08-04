c0e674a890fe43f74181c6b3f424db5a922e2e92
Sergey <vor.ser87@gmail.com>
2018-08-05 01:18:33 +0300

    translate rezervation

--- resources/views/frontend/sections/end_reservation.blade.php
+++ resources/views/frontend/sections/end_reservation.blade.php
@@ -7,13 +7,13 @@
                 </div>
                 <div class="row text-center">
                     <div class="col">
-                        <h2 class="section-header-huge pt-0">дякуємо!</h2>
-                        <h6 class="section-header-small text-lowercase">наш менеджер невдовзі перетелефонує вам</h6>
+                        <h2 class="section-header-huge pt-0">{{ trans('base.thanks')}}!</h2>
+                        <h6 class="section-header-small text-lowercase">{{ trans('base.manager_call')}}</h6>
                     </div>
                 </div>
                 <div class="row  justify-content-center my-5">
                     <div class="col-md-6 col-10">
-                        <button type="button" class="btn btn-yellow" data-dismiss="modal">Зрозуміло</button>
+                        <button type="button" class="btn btn-yellow" data-dismiss="modal">{{ trans('base.understand')}}</button>
                     </div>
                 </div>
             </div>
